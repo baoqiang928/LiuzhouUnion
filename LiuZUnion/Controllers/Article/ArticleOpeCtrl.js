ea00ab5c27c4e5fa641bea61acb48c5e1eda6aef
baoqiang <baoqiang928@126.com>
2018-04-24 22:42:06 -0600

    文章信息管理 富文本输入

--- LiuZUnion/Controllers/Article/ArticleOpeCtrl.js
+++ LiuZUnion/Controllers/Article/ArticleOpeCtrl.js
@@ -1,5 +1,33 @@
 angular.module("myApp")
     .controller('ArticleOpeCtrl', function ($scope, $location, requestService, $stateParams, $state) {
+        $scope._simpleConfig = {
+            //初始化编辑器内容  
+            content: '<p>test1</p>',
+            //是否聚焦 focus默认为false  
+            focus: true,
+            //首行缩进距离,默认是2em  
+            indentValue: '2em',
+            //初始化编辑器宽度,默认1000  
+            initialFrameWidth: '100%',
+            //初始化编辑器高度,默认320  
+            initialFrameHeight: 720,
+            //编辑器初始化结束后,编辑区域是否是只读的，默认是false  
+            readonly: false,
+            //启用自动保存  
+            enableAutoSave: false,
+            //自动保存间隔时间， 单位ms  
+            saveInterval: 500,
+            //是否开启初始化时即全屏，默认关闭  
+            fullscreen: false,
+            //图片操作的浮层开关，默认打开  
+            imagePopup: true,
+            //提交到后台的数据是否包含整个html字符串  
+            allHtmlEnabled: false,
+            //额外功能添加                 
+            functions: ['map', 'insertimage', 'insertvideo', 'attachment',
+            , 'insertcode', 'webapp', 'template',
+            'background', 'wordimage']
+        };
         var Sources = "Articles";
         $scope.data = {
             DicID: "",
@@ -16,7 +44,8 @@ angular.module("myApp")
         }
 
         $scope.Save = function () {
-
+            UE.getEditor('idTest').setContent(" ", true);
+            $scope.$apply();//必需手动进行脏值检测,否则数据无法刷新到界面  
             if (!$('#validation-form').valid()) {
                 return false;
             }
--- LiuZUnion/Controllers/CauseEffect/ImproveProblem.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('myApp')
-    .controller('ImproveProblemCtrl', function ($scope, $location, requestService, $state, locals) {
-
-
-    });
-
